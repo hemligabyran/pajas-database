6233c9f8efe3c6be1468c51559ca75d1fd0c3004
Lilleman <lilleman@larvit.se>
2013-04-08 16:22:02 +0200

    Initial commit

---
+++ classes/pajas/model.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+abstract class Pajas_Model {
+
+	// Database instance
+	public $pdo = 'default';
+
+	/**
+	 * Loads the database.
+	 *
+	 *     $model = new Foo_Model($db);
+	 *
+	 * @param   mixed  Database instance object or string
+	 * @return  void
+	 */
+	public function __construct($instance_name = NULL)
+	{
+		if ($instance_name !== NULL)
+		{
+			// Set the database instance name
+			$this->pdo = $instance_name;
+		}
+
+		if (is_string($this->pdo))
+		{
+			// Load the database
+			$this->pdo = Pdo::instance($this->pdo);
+		}
+	}
+
+}
